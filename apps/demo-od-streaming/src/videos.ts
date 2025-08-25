191ddf8bab97e022e6135b848d2646291984ad9f
Lewis B <lewis@ikigaistudios.eu>
2025-08-25 18:33:10 +0700

    chore: add missing tsconfig

--- apps/demo-od-streaming/src/videos.ts
+++ apps/demo-od-streaming/src/videos.ts
@@ -2,10 +2,72 @@ export type Video = {
   id: number;
   thumbnail: string;
   src: string;
+  title: string;
+  price: string;
 };
 
 export const mockVideos: Video[] = [
-  { id: 1, thumbnail: "/assets/test1.jpg", src: "/assets/sample.mp4" },
-  { id: 2, thumbnail: "/assets/test1.jpg", src: "/assets/sample.mp4" },
-  { id: 3, thumbnail: "/assets/test1.jpg", src: "/assets/sample.mp4" },
+  {
+    id: 1,
+    thumbnail: "/assets/test1.jpg",
+    src: "/assets/sample.mp4",
+    title: "Futuristic Cyberspace",
+    price: "3.40",
+  },
+  {
+    id: 2,
+    thumbnail: "/assets/test1.jpg",
+    src: "/assets/sample.mp4",
+    title: "Cyberpunk Streets",
+    price: "5.60",
+  },
+  {
+    id: 3,
+    thumbnail: "/assets/test1.jpg",
+    src: "/assets/sample.mp4",
+    title: "Virtual Reality Odyssey",
+    price: "2.60",
+  },
+  {
+    id: 4,
+    thumbnail: "/assets/test1.jpg",
+    src: "/assets/sample.mp4",
+    title: "Neon Nights",
+    price: "3.10",
+  },
+  {
+    id: 5,
+    thumbnail: "/assets/test1.jpg",
+    src: "/assets/sample.mp4",
+    title: "Midnight Menscape",
+    price: "4.10",
+  },
+  {
+    id: 6,
+    thumbnail: "/assets/test1.jpg",
+    src: "/assets/sample.mp4",
+    title: "Dark Nights",
+    price: "1.90",
+  },
+  {
+    id: 7,
+    thumbnail: "/assets/test1.jpg",
+    src: "/assets/sample.mp4",
+    title: "Neon Odyssey",
+    price: "5.60",
+  },
+  {
+    id: 8,
+    thumbnail: "/assets/test1.jpg",
+    src: "/assets/sample.mp4",
+    title: "Neon Rovide",
+    price: "6.20",
+  },
+  {
+    id: 9,
+    thumbnail: "/assets/test1.jpg",
+    src: "/assets/sample.mp4",
+    title: "Addostrum",
+    price: "3.40",
+  },
 ];
